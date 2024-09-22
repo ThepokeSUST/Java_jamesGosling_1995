1d70612d98f700868f43139845d392bff2e90257
dipok <dipoknathdipu@gmail.com>
2024-09-22 14:34:49 +0600

    type casting

---
+++ typecast.java
@@ -0,0 +1,38 @@
+
+
+public class typecast {
+      
+      public static void main(String arg[]){
+         //byte = 1 byte= 8 bit;
+              //minbyte = -128;
+              //maxbyte = 127;
+          byte by=12;
+          //if i wanna store by in int type variable.It will works. Because ,int type>>byte type...
+          int a=by;  //This is implicit conversion
+          System.out.println(a);
+          
+          //int = 4 byte = 32 bit
+               //minint = -2147483648
+               //maxint =  2147483647  
+           int in=24;
+           //if i wanna store in(variable) in byte type .It will throw error. Because int type>>byte type.
+           //byte b=in; this will show (cannot convert from int to byte) ...
+           //But i can store in float variable...Because float type == int type(in size)...
+           float f=in;
+           f=2147483647;
+           System.out.println(f);
+           
+           //now if you store float in int ,it will throw error.
+           //so you should explicitly do type cast...
+
+           //int ii=f; this will not work...
+           int ii= (int)f;
+           System.out.println(ii);
+           
+           
+           
+         
+       
+       
+      }
+}
